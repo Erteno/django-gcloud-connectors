1541f03d2e540debe0b14fc52fbbe02e2e8298c0
Tina Limbu <tina.limbu@potatolondon.com>
2021-06-04 15:15:39 +0100

    Bump to alpha version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ AUTHOR_EMAIL = "mail@p.ota.to"
 if os.environ.get('CI_COMMIT_TAG'):
     VERSION = os.environ['CI_COMMIT_TAG']
 else:
-    VERSION = '0.3.3'
+    VERSION = '0.3.4a'
 
 setup(
     name=NAME,
